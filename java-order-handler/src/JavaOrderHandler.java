5c7bd9eede0dba117cd0cdb3d7a76bf86f1d7875
Cem Reis <76175330+cemreis0@users.noreply.github.com>
2023-07-07 18:57:42 +0300

    Update JavaOrderHandler.java

--- java-order-handler/src/JavaOrderHandler.java
+++ java-order-handler/src/JavaOrderHandler.java
@@ -4,8 +4,6 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class JavaOrderHandler {
     public static void main(String[] args) {
         /*
